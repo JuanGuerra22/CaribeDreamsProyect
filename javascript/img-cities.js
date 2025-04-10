d4c2920af1dce68293d94ade796da37f1355fec6
Juan Guerra <juan.l.guerrar@gmail.com>
2025-04-09 20:09:50 -0500

    Index modificado

--- javascript/img-cities.js
+++ javascript/img-cities.js
@@ -1,4 +1,4 @@
-const selectCity = document.getElementById('ciudad');
+const selectCity = document.getElementById('departamento');
 const contMonteria = document.getElementById('cont-monteria');
 const contCartagena = document.getElementById('cont-cartagena');
 const contSincelejo = document.getElementById('cont-sincelejo');
@@ -19,11 +19,11 @@ function mostrarCiudad(){
         titleCartagena.classList.add('hidden');
         titleSincelejo.classList.add('hidden');
         titleSanAndres.classList.add('hidden');
-        if(selectCity.value === "monteria"){
+        if(selectCity.value === "cordoba"){
             contMonteria.classList.add('visible');
             titleMonteria.classList.remove('hidden');
             contMonteria.scrollIntoView({ behavior: 'smooth' });
-        } else if(selectCity.value === "cartagena"){
+        } else if(selectCity.value === "bolivar"){
             contCartagena.classList.add('visible')
             titleCartagena.classList.remove('hidden')
             contCartagena.scrollIntoView({ behavior: 'smooth' });
@@ -31,7 +31,7 @@ function mostrarCiudad(){
             contSanAndres.classList.add('visible')
             titleSanAndres.classList.remove('hidden')
             contSanAndres.scrollIntoView({ behavior: 'smooth' });
-        }else if(selectCity.value === "sincelejo"){
+        }else if(selectCity.value === "sucre"){
             contSincelejo.classList.add('visible')
             titleSincelejo.classList.remove('hidden')
             contSincelejo.scrollIntoView({ behavior: 'smooth' });
@@ -39,4 +39,53 @@ function mostrarCiudad(){
     })
 }
 
-mostrarCiudad();
+// mostrarCiudad();
+
+// const servicesCont = document.querySelectorAll('.services-cont')
+
+// const selectService = document.getElementById('servicios');
+// const titleCordHotel = document.getElementById('title-cord-hotel');
+// const servicesCordoba = document.getElementById('serv-cordoba');
+// const titleCordobaHotel = document.getElementById('title-cord-hotel');
+
+// const btnSearch = document.getElementById('btn-search-bar');
+
+// function mostrarServicio(){
+//         titleCordobaHotel.classList.add('hidden');
+//         servicesCordoba.classList.remove('visible');
+//         if(selectService.value === 'accommodation'){
+//             servicesCordoba.classList.add('visible');
+//             titleCordobaHotel.classList.remove('hidden');
+//             titleCordobaHotel.scrollIntoView({ behavior: 'smooth' });
+//         }  
+    
+// }
+
+// btnSearch.addEventListener('click', mostrarServicio)
+
+
+
+const titleCordHotel = document.getElementById('title-cord-hotel');
+const selectDepart = document.getElementById('departamento');
+const selectService = document.getElementById('servicios');
+const contServicios = document.querySelectorAll('.services-cont');
+const btnSearch = document.getElementById('btn-search-bar');
+
+
+
+function mostrarContenido(){
+    const departamentos = selectDepart.value;
+    const servicios = selectService.value;
+    const idMostrar = `${departamentos}-${servicios}`;
+    const contMostrar = document.getElementById(idMostrar);
+    console.log(contMostrar)
+     if(contMostrar){
+        contMostrar.classList.add('visible')
+        titleCordHotel.classList.remove('hidden')
+        titleCordHotel.scrollIntoView({ behavior: 'smooth' });
+    }
+   
+}
+btnSearch.addEventListener('click', mostrarContenido)
+
+
